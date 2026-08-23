d53c64d53f06d6e6ac55d63bc92010286069718b
user <sergeivanov827@gmail.com>
2026-08-23 19:32:18 +0300

    add application of migrations using goose
    Migration files are embedded in code using "embed" package and initDB
    method execute this migrations.

--- go.mod
+++ go.mod
@@ -6,10 +6,10 @@ require (
 	github.com/JJJJJJack/go-template-docx v1.4.6
 	github.com/Oudwins/tailwind-merge-go v0.2.1
 	github.com/a-h/templ v0.3.1020
-	github.com/jackc/pgx/v5 v5.8.0
+	github.com/jackc/pgx/v5 v5.10.0
 	github.com/joho/godotenv v1.5.1
 	github.com/nyaruka/phonenumbers v1.6.11
-	github.com/paulmach/orb v0.12.0
+	github.com/paulmach/orb v0.13.0
 	github.com/shopspring/decimal v1.4.0
 	github.com/xuri/excelize/v2 v2.10.1
 )
@@ -19,15 +19,19 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/mfridman/interpolate v0.0.2 // indirect
+	github.com/pressly/goose/v3 v3.27.3 // indirect
 	github.com/richardlehane/mscfb v1.0.6 // indirect
 	github.com/richardlehane/msoleps v1.0.6 // indirect
+	github.com/sethvargo/go-retry v0.4.0 // indirect
 	github.com/tiendc/go-deepcopy v1.7.2 // indirect
 	github.com/xuri/efp v0.0.1 // indirect
 	github.com/xuri/nfp v0.0.2-0.20250530014748-2ddeb826f9a9 // indirect
-	golang.org/x/crypto v0.48.0 // indirect
-	golang.org/x/exp v0.0.0-20250305212735-054e65f0b394 // indirect
-	golang.org/x/net v0.51.0 // indirect
-	golang.org/x/sync v0.19.0 // indirect
-	golang.org/x/text v0.34.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/crypto v0.54.0 // indirect
+	golang.org/x/exp v0.0.0-20260718201538-764159d718ef // indirect
+	golang.org/x/net v0.57.0 // indirect
+	golang.org/x/sync v0.22.0 // indirect
+	golang.org/x/text v0.40.0 // indirect
 	google.golang.org/protobuf v1.36.11 // indirect
 )
--- internal/db/db.go
+++ internal/db/db.go
@@ -2,25 +2,31 @@ package db
 
 import (
 	"context"
+	"embed"
 	"errors"
 	"fmt"
 
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5/stdlib"
+	"github.com/pressly/goose/v3"
 	"github.com/s-588/tms/internal/config"
 	"github.com/s-588/tms/internal/db/generated"
 )
 
 var (
 	ErrIncorrectPhone = errors.New("phone have incorrect format")
-    ErrDuplicateEmail = errors.New("email already exists")
-    ErrDuplicatePhone = errors.New("phone already exists")
+	ErrDuplicateEmail = errors.New("email already exists")
+	ErrDuplicatePhone = errors.New("phone already exists")
 
-    ErrDuplicateLicense = errors.New("license plate already exists")
+	ErrDuplicateLicense = errors.New("license plate already exists")
 
-    ErrDuplicatePrice = errors.New("price configuration (cargo type, weight, distance) already exists")
-	
+	ErrDuplicatePrice = errors.New("price configuration (cargo type, weight, distance) already exists")
+
 	ErrDuplicateNodeAddress = errors.New("node with this address already exists")
 )
+
+//go:embed migrations/*.sql
+var embeddedMigrations embed.FS
 
 type DB struct {
 	queries *generated.Queries
@@ -35,11 +41,13 @@ func New(ctx context.Context, cfg config.DBConfig) (DB, error) {
 	}
 
 	quieries := generated.New(pool)
-	return DB{
+	db := DB{
 		queries: quieries,
 		cfg:     cfg,
 		pool:    pool,
-	}, nil
+	}
+	db.initDB()
+	return db, nil
 }
 
 func getConnStr(cfg config.DBConfig) string {
@@ -47,6 +55,20 @@ func getConnStr(cfg config.DBConfig) string {
 		cfg.User, cfg.Password, cfg.Addr, cfg.Port, cfg.DB)
 }
 
-func (db DB) Close() {
+func (db *DB) Close() {
 	db.pool.Close()
+}
+
+func (db *DB) initDB() error {
+	goose.SetBaseFS(embeddedMigrations)
+	if err := goose.SetDialect(string(goose.DialectPostgres)); err != nil {
+		return fmt.Errorf("set dialect: %w", err)
+	}
+
+	dbFromPool := stdlib.OpenDBFromPool(db.pool)
+
+	if err := goose.Up(dbFromPool, "migrations"); err != nil {
+		return fmt.Errorf("execute migrations: %w", err)
+	}
+	return nil
 }
